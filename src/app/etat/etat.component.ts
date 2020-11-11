57925c7c81000c08731fc6f24bca0732141e2952
mohammed <med.eddaghal@gmail.com>
2020-11-11 23:17:13 +0100

    commit 11/11/2020

--- src/app/etat/etat.component.ts
+++ src/app/etat/etat.component.ts
@@ -85,17 +85,76 @@ export class EtatComponent implements OnInit {
     this.pagedItems = this.etats.slice(this.pager.startIndex, this.pager.endIndex + 1);
   }
 
-  checkType(value: any): number {
+  checkType(value: any,heading:number,speed:number): number {
     if (typeof (value) === 'string') {
       if (value === '+24h') {
-        this.etatLogo="../assets/status/stop_small.png"
+        this.etatLogo="../assets/status/r_marker_blue.png"
         return 1;
       } else {
         this.etatLogo="../assets/status/marker_blue_parking.png"
         return 0;
       }
     }
-    this.etatLogo="../assets/status/marker_green_n.png" 
+    if(speed<3)
+      this.etatLogo="../assets/status/stop_small.png";
+    else if(speed<60){
+      if(heading==0)
+        this.etatLogo="../assets/status/marker_green.png";
+      else if(heading<90)
+        this.etatLogo="../assets/status/marker_green_ne.png";
+      else if(heading==90)
+        this.etatLogo="../assets/status/marker_green_e.png";
+      else if(heading<180)
+        this.etatLogo="../assets/status/marker_green_se.png";
+      else if(heading==180)
+        this.etatLogo="../assets/status/marker_green_s.png";
+      else if(heading<270)
+        this.etatLogo="../assets/status/marker_green_sw.png";
+      else if(heading==270)
+        this.etatLogo="../assets/status/marker_green_w.png";
+      else if(heading<360)
+        this.etatLogo="../assets/status/marker_green_nw.png";
+      else
+        this.etatLogo="../assets/status/marker_green_n.png";
+    }else if(speed<100){
+      if(heading==0)
+        this.etatLogo="../assets/status/marker_grey.png";
+      else if(heading<90)
+        this.etatLogo="../assets/status/marker_grey_ne.png";
+      else if(heading==90)
+        this.etatLogo="../assets/status/marker_grey_e.png";
+      else if(heading<180)
+        this.etatLogo="../assets/status/marker_grey_se.png";
+      else if(heading==180)
+        this.etatLogo="../assets/status/marker_grey_s.png";
+      else if(heading<270)
+        this.etatLogo="../assets/status/marker_grey_sw.png";
+      else if(heading==270)
+        this.etatLogo="../assets/status/marker_grey_w.png";
+      else if(heading<360)
+        this.etatLogo="../assets/status/marker_grey_nw.png";
+      else
+        this.etatLogo="../assets/status/marker_grey_n.png";
+    }else{
+      if(heading==0)
+        this.etatLogo="../assets/status/marker_yellow.png";
+      else if(heading<90)
+        this.etatLogo="../assets/status/marker_yellow_ne.png";
+      else if(heading==90)
+        this.etatLogo="../assets/status/marker_yellow_e.png";
+      else if(heading<180)
+        this.etatLogo="../assets/status/marker_yellow_se.png";
+      else if(heading==180)
+        this.etatLogo="../assets/status/marker_yellow_s.png";
+      else if(heading<270)
+        this.etatLogo="../assets/status/marker_yellow_sw.png";
+      else if(heading==270)
+        this.etatLogo="../assets/status/marker_yellow_w.png";
+      else if(heading<360)
+        this.etatLogo="../assets/status/marker_yellow_nw.png";
+      else
+        this.etatLogo="../assets/status/marker_yellow_n.png";
+    }
     return -1;
   }
 
