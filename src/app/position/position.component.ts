529617242b688d8aaf6a09f72f8c573943fe5f8f
AbdelDer <AbdelDer@github.com>
2020-12-21 18:32:43 +0100

    filter added on click (device)

--- src/app/position/position.component.ts
+++ src/app/position/position.component.ts
@@ -1,9 +1,9 @@
 import { AdminService } from "./../services/admin.service";
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from "../services/auth.service";
 import { Device } from "../models/device.model";
-import { Icon, Map, Marker} from 'leaflet';
-import { Observable } from "rxjs";
+import { Icon, Map, Marker } from 'leaflet';
+import { Observable, Subscription } from "rxjs";
 import { interval } from "rxjs";
 import { map } from "rxjs/operators";
 
@@ -12,17 +12,15 @@ import { map } from "rxjs/operators";
   templateUrl: './position.component.html',
   styleUrls: ['./position.component.css']
 })
-export class PositionComponent implements OnInit {
+export class PositionComponent implements OnInit, OnDestroy {
   isAdmin: boolean;
   isSearching: boolean = false;
-  devices: Device[];
+  devices: Device[] = [];
+  markers: CMarker[] = [];
+  timer: Subscription;
+  devicesSelected: any = [];
   private map: Map;
   private zoom: number;
-  private smallIcon = new Icon({
-    iconUrl: '/assets/status/marker_blue_parking.png',
-    iconSize: [26, 30],
-    iconAnchor: [14, 4],
-  });
   private parc = {
     lat: 34.033759,
     lng: -5.009296
@@ -32,6 +30,16 @@ export class PositionComponent implements OnInit {
   ngOnInit(): void {
     this.isAdmin = this.adminService.isAdmin;
     this.updateDevices();
+    this.timer = interval(1000).subscribe(() => {
+      // console.log('say hello baby');
+      this.updateDevices();
+    });
+  }
+  /**
+   * this method called on destroy, we use it to close our subscription
+   */
+  ngOnDestroy(): void {
+    this.timer.unsubscribe();
   }
 
   receiveMap(map: Map) {
@@ -39,11 +47,25 @@ export class PositionComponent implements OnInit {
   }
 
   setMarkers() {
+    if (this.markers.length != 0) {
+      this.markers.every((marker) => marker.marker.remove());
+      this.markers.length = 0;
+    }
     this.devices.forEach(device => {
-      const marker = new Marker([device.latitude, device.longitude], { icon: this.smallIcon });
-      marker.addTo(this.map).bindPopup('text');
+     if(this.devicesSelected.find(deviceID => device.deviceID == deviceID)) {
+      this.markers.push({
+        "marker": new Marker([device.latitude, device.longitude], {
+          icon: new Icon({
+            iconUrl: device.icon(),
+            iconSize: [26, 30],
+            iconAnchor: [14, 4],
+          })
+        }),
+        "deviceID": device.deviceID
+      });
+     }
     });
-    interval(3000).subscribe((val) => { console.log('called'); this.devices[0].activity_time='+10,l'; });
+    this.markers.every((marker) => marker.marker.addTo(this.map));
   }
 
   receiveZoom(zoom: number) {
@@ -54,11 +76,50 @@ export class PositionComponent implements OnInit {
     this.adminService.getDevicesPosition(this.authService.user.accountID, this.authService.user.userID).pipe(
       map((data: Device[]) => data.map(device => new Device().deserialize(device)))
     ).subscribe(
-        (response) => {
-          this.devices = response;
-          this.setMarkers();
-        },
-        error => null,
-      );
+      (response) => {
+        if (this.devices.length == 0) {
+          response.forEach((device, index) => {
+            this.devicesSelected[index] = device.deviceID;
+          });
+          console.log('setting array first time!!!!');
+        }
+        this.devices = response;
+        this.setMarkers();
+      },
+      error => null,
+    );
   }
+
+  onNgModelChange(deviceID) {
+    const index = this.devicesSelected.indexOf(deviceID);
+    if (index == -1) {
+      this.devicesSelected.push(deviceID);
+      const cMarkerIndex = this.markers.findIndex(marker => marker.deviceID == deviceID);
+      this.markers[cMarkerIndex].marker.remove();
+      this.markers.splice(cMarkerIndex, 1);
+
+    } else {
+      this.devicesSelected.splice(index, 1);
+      const device = this.devices.find(device => device.deviceID = deviceID);
+      this.markers.push({
+        "marker": new Marker([device.latitude, device.longitude], {
+          icon: new Icon({
+            iconUrl: device.icon(),
+            iconSize: [26, 30],
+            iconAnchor: [14, 4],
+          })
+        }),
+        "deviceID": device.deviceID
+      });
+      this.markers[this.markers.length].marker.addTo(this.map);
+    }
+  }
+}
+
+/**
+ * This interface help us to respect the structure in our markers array
+ */
+export interface CMarker {
+  marker: Marker,
+  deviceID: String
 }
