7c7a72c192392e6c65a01963b7d2c29d00647de3
AbdelDer <AbdelDer@github.com>
2021-01-01 13:05:10 +0100

    listing devices done

--- src/app/models/device.model.ts
+++ src/app/models/device.model.ts
@@ -1,26 +1,27 @@
 import { Deserializable } from "./deserializable.model";
 
 export class Device implements Deserializable {
-    private deviceID: DeviceID;
-    private equipmentType: String;
-    private equipmentStatus: String;
-    private vehicleModel: String;
-    private vehicleID: String;
-    private licenseExpire: number;
-    private driverID: String;
-    private driverStatus: number;
-    private uniqueID: String;
-    private serialNumber: String;
-    private simPhoneNumber: String;
-    private imeiNumber: String;
-    private odometerOffsetKM: number;
-    private engineHoursOffset: number;
-    private isActive: number;
-    private displayName: String;
-    private description: String;
-    private lastUpdateTime: number;
-    private creationTime: number;
-
+    deviceID: DeviceID;
+    equipmentType: String;
+    equipmentStatus: String;
+    vehicleModel: String;
+    vehicleID: String;
+    licenseExpire: number;
+    driverID: String;
+    driverStatus: number;
+    uniqueID: String;
+    serialNumber: String;
+    simPhoneNumber: String;
+    imeiNumber: String;
+    odometerOffsetKM: number;
+    engineHoursOffset: number;
+    isActive: number;
+    displayName: String;
+    description: String;
+    lastUpdateTime: number;
+    creationTime: number;
+    remainingTime: number;
+    
     deserialize(input: any) {
         Object.assign(this, input);
         return this;
