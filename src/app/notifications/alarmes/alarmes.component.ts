670b25d9638aa58b0cb4042900b877b3ad18ecbf
AbdelDer <AbdelDer@github.com>
2021-02-07 12:18:18 +0100

    vehicleModel added to alarm

--- src/app/notifications/alarmes/alarmes.component.ts
+++ src/app/notifications/alarmes/alarmes.component.ts
@@ -16,6 +16,7 @@ export class AlarmesComponent implements OnInit {
 
   vehicles: Vehicle[];
   alarm: Alarm = new Alarm();
+  vehicleModel: string = "";
 
   constructor(private adminService: AdminService, private authService: AuthService, private modalService: NgbModal) { }
 
@@ -44,23 +45,24 @@ export class AlarmesComponent implements OnInit {
         );
         this.alarm = new Alarm();
       },
-      (reason) => { 
+      (reason) => {
         this.alarm = new Alarm();
       }
     );
   }
 
-  getAlarmConfig(content, deviceID: string) {
+  getAlarmConfig(content, deviceID: string, vehicleModel: string) {
     this.adminService.getAlarmConfig(this.authService.user.accountID, deviceID, this.authService.user.userID).pipe(
       map((alarm: any) => new Alarm().deserialize(alarm))
     ).subscribe(
       response => {
-        if(response.alarmID != null) {
+        if (response.alarmID != null) {
           this.alarm = response;
         } else {
           this.alarm = new Alarm();
           this.alarm.alarmID = new AlarmID(this.authService.user.accountID, this.authService.user.userID, deviceID);
         }
+        this.vehicleModel = vehicleModel;
         this.open(content);
       },
       error => {
